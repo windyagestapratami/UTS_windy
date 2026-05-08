86c1b5c3396dcec6e0dbe2e1962b5e33f1f1d8ba
windyagestapratami <niputuwindyagestapratami@gmail.com>
2026-05-08 14:11:35 +0800

    Create Tunai.java

---
+++ Tunai.java
@@ -0,0 +1,16 @@
+public class Tunai extends pembayaran{
+    private double nominal;
+
+            public Pembayaran(double nominal){
+            this.nominal = nama;
+            super(String nama, int nomorMeja, double totalTagihan);
+            this.nama = nama;
+            this.nomorMeja;
+            this.totalTagihan;
+            }
+
+     @Override
+    public void lakukan pemnbayaran() {
+        System.out.println("Pembayaran Tunai telah berhasil dilakukan"); 
+
+}
